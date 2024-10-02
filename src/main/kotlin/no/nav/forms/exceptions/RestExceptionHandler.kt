51622bbdd7c6974190e667f48d885aaf3e05b374
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2024-10-02 21:33:07 +0200

    Log exception

--- src/main/kotlin/no/nav/forms/exceptions/RestExceptionHandler.kt
+++ src/main/kotlin/no/nav/forms/exceptions/RestExceptionHandler.kt
@@ -1,6 +1,8 @@
 package no.nav.forms.exceptions
 
 import no.nav.forms.model.ErrorResponseDto
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -9,16 +11,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 @ControllerAdvice
 class RestExceptionHandler {
 
+	val logger: Logger = LoggerFactory.getLogger(javaClass)
+
 	@ExceptionHandler
 	fun handleResourceNotFound(exception: ResourceNotFoundException): ResponseEntity<ErrorResponseDto> {
-		// TODO log exception
+		logger.info("${exception.message} (id=${exception.resourceId})", exception)
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponseDto(exception.message))
 	}
 
 	@ExceptionHandler
 	fun handleGenericException(exception: Exception): ResponseEntity<ErrorResponseDto> {
-		// TODO log exception
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorResponseDto("Something went wrong"))
+		val responseErrorMessage = "Something went wrong"
+		logger.warn("$responseErrorMessage: ${exception.message}", exception)
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorResponseDto(responseErrorMessage))
 	}
 
 }
