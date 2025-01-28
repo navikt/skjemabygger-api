d5a45c55fc8ce23d4c772444ba851d116543194f
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2025-01-28 14:04:24 +0100

    Some texts are longer than 1024 chars

---
+++ src/main/resources/db/migration/V2.1__translations_max_length.sql
@@ -0,0 +1,4 @@
+ALTER TABLE form_translation ALTER COLUMN key TYPE VARCHAR(2048);
+ALTER TABLE form_translation_revision ALTER COLUMN nb TYPE VARCHAR(2048);
+ALTER TABLE form_translation_revision ALTER COLUMN nn TYPE VARCHAR(2048);
+ALTER TABLE form_translation_revision ALTER COLUMN en TYPE VARCHAR(2048);
